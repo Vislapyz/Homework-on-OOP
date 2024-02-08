f55fa40f55148402885aa841b268800a8019e49e
Mihail <vislapyz@mail.ru>
2024-02-08 23:34:54 +0300

    Writing tests

---
+++ tests/tests_utils.py
@@ -0,0 +1,21 @@
+import pytest
+from src.utils import Category, Product
+
+@pytest.fixture
+def category_tv():
+    return Category('Телевизор', 'Современные телевизоры' , ['LG' , 'Xiaomi', 'Samsung'])
+
+def test_init(category_tv):
+    assert category_tv.title =='Телевизор'
+    assert category_tv.description == 'Современные телевизоры'
+    assert category_tv.products == ['LG' , 'Xiaomi', 'Samsung']
+
+@pytest.fixture
+def product_xiaomi():
+    return Product('Xiaomi TV2', 'Телевизор 32, смарт ТВ', '16000.00', '3')
+
+def test_init(product_xiaomi):
+    assert product_xiaomi.title == 'Xiaomi TV2'
+    assert product_xiaomi.description == 'Телевизор 32, смарт ТВ'
+    assert product_xiaomi.price == '16000.00'
+    assert product_xiaomi.in_stock == '3'
