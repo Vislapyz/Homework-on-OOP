87e79c94b2d968b3c084002454446dad8ded0889
Mihail <vislapyz@mail.ru>
2024-02-08 22:11:07 +0300

    Method for initializing an instance of a class

--- src/utils.py
+++ src/utils.py
@@ -6,11 +6,29 @@ class Category:
     description: str
     products: list
 
+    def __init__(self, title, description, products):
+        """
+        Метод для инициализации экземпляра класса
+        """
+        self.title = title
+        self.description = description
+        self.products = products
+
+
 class Product:
     """
     Класс продукций и количества
     """
     title: str
     description: str
-    price: int
+    price: float
     in_stock: int
+
+    def __init__(self, title, description, price, in_stock):
+        """
+        Метод для инициализации экземпляра класса
+        """
+        self.title = title
+        self.description = description
+        self.price = price
+        self.in_stock = in_stock
