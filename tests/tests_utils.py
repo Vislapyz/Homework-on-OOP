7fa57e1ac5e06fbdd5911585b7c59e77f835c47b
Mihail <vislapyz@mail.ru>
2024-02-09 20:45:23 +0300

    Error correction

--- tests/tests_utils.py
+++ tests/tests_utils.py
@@ -1,18 +1,24 @@
 import pytest
 from src.utils import Category, Product
 
+
 @pytest.fixture
 def category_tv():
-    return Category('Телевизор', 'Современные телевизоры' , ['LG' , 'Xiaomi', 'Samsung'])
+    return Category('Телевизор', 'Современные телевизоры', ['LG', 'Xiaomi', 'Samsung'])
+
 
 def test_init(category_tv):
-    assert category_tv.title =='Телевизор'
+    assert category_tv.title == 'Телевизор'
     assert category_tv.description == 'Современные телевизоры'
-    assert category_tv.products == ['LG' , 'Xiaomi', 'Samsung']
+    assert category_tv.products == ['LG', 'Xiaomi', 'Samsung']
+    assert Category.total_categories == 1
+    assert Category.total_unique_products == 3
+
 
 @pytest.fixture
 def product_xiaomi():
     return Product('Xiaomi TV2', 'Телевизор 32, смарт ТВ', '16000.00', '3')
+
 
 def test_init(product_xiaomi):
     assert product_xiaomi.title == 'Xiaomi TV2'
