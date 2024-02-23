a760a783ec937d77d4e079f3654017a504469e73
Mihail <vislapyz@mail.ru>
2024-02-23 09:52:06 +0300

    abstract class task 1

--- src/product.py
+++ src/product.py
@@ -1,4 +1,25 @@
-class Product:
+from abc import ABC, abstractmethod
+
+
+class AllProducts(ABC):
+    @abstractmethod
+    def __init__(self):
+        pass
+
+    @abstractmethod
+    def __str__(self):
+        pass
+
+    @abstractmethod
+    def __add__(self, other):
+        pass
+
+    class ForAllClassesMixin:
+        def __repr__(self):
+            pass
+
+
+class Product(AllProducts):
     """
     Класс продукций и количества
     """
