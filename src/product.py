ac60ccce2a528ef99c87fad336666d0e1d4782ba
Mihail <vislapyz@mail.ru>
2024-02-24 21:47:37 +0300

    task 2 error correction

--- src/product.py
+++ src/product.py
@@ -16,8 +16,10 @@ class AllProducts(ABC):
 
     class ForAllClassesMixin:
         def __repr__(self):
-            return f'создан объект{self.__class__.__name__}'
-
+            object_attributes: = ''
+            for k, v in self.__dict__.items():
+                object_attributes += f'{k}: {v},'
+            return object_attributes
 
 class Product(AllProducts):
     """
